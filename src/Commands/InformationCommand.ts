6f16b2091336fc94ccab11a59aec8c8223934bb5
haydenykh <yonghayden68@gmail.com>
2024-09-03 21:50:19 +0800

    style: bold text

--- src/Commands/InformationCommand.ts
+++ src/Commands/InformationCommand.ts
@@ -33,9 +33,9 @@ export default class InformationCommand extends Command {
         const infoEmbed = new EmbedBuilder()
             .setTitle(`👋 Hi,`)
             .setDescription(
-                `- I am a bot whom provides predominantly science quizzes from Malaysia's governmental secondary schools with both :flag_my: **Malay (Bahasa Melayu)** and :flag_gb: **English** support.\n- I was created by [@haydenykh](https://discord.com/users/900561055472840724) using discord.js module and TypeScript.\n- If you want to add new questions or fix my errors, head to the [CONTRIBUTION.md file](https://github.com/haydenykh/SainsOrScienceBot/blob/main/CONTRIBUTE.md).\n- I was also created on ${clientCreatedAt.toLocaleDateString(
+                `- I am a bot whom provides predominantly **science quizzes** from Malaysia's governmental secondary schools with both **:flag_my: Malay (Bahasa Melayu)** and **:flag_gb: English support**.\n- I was created by [@haydenykh](https://discord.com/users/900561055472840724) using the **discord.js module** and **TypeScript**.\n- If you want to **add new questions** or **fix my errors**, head to the **[CONTRIBUTION.md file](https://github.com/haydenykh/SainsOrScienceBot/blob/main/CONTRIBUTE.md)** on my **[GitHub repository](https://github.com/haydenykh/SainsOrScienceBot)**.\n- I was also created on **${clientCreatedAt.toLocaleDateString(
                     "en-GB"
-                )}, that was approximately ${differenceInDays} days ago.`
+                )}**, that was approximately **${differenceInDays} days ago**.`
             )
             .setColor("Random")
             .setThumbnail(
