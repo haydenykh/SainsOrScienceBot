458e3f6ec20a115a850d7c997a203a332c80f900
haydenykh <yonghayden68@gmail.com>
2024-10-02 15:13:58 +0800

    feat: Added `express`

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 import "dotenv/config";
 import ExtendedClient from "./Base/Classes/ExtendedClient.js";
+import express from "express"
 
 new ExtendedClient().Init();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: express.Request, res: express.Response) => {
+	res.send('Bot is running!');	
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
+});
